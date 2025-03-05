844179ff5d5410d628ecdbb638d1d057b7902f3e
Jose Espejo <joseespejo@yahoo.com>
2025-03-05 20:29:11 +0000

    feat: add search icon to input field in SearchBar component

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addCity } from "../store/weatherSlice";
+import { Search } from "lucide-react";
 
 function SearchBar() {
   const [search, setSearch] = useState("");
@@ -24,13 +25,16 @@ function SearchBar() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={search}
-        onChange={(event) => setSearch(event.target.value)}
-        placeholder="Add a city"
-        className="w-full rounded-md border border-gray-600 p-2"
-      />
+      <div className="relative">
+        <Search className="absolute top-2.5 left-3 h-5 w-5 text-gray-500" />
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          placeholder="Add a city"
+          className="w-full rounded-md border border-gray-600 p-2 pl-9"
+        />
+      </div>
     </form>
   );
 }
