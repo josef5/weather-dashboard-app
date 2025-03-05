377f06062250361ea1341a4632e56ce32c1b8c00
Jose Espejo <joseespejo@yahoo.com>
2025-03-05 18:07:41 +0000

    feat: implement city addition in SearchBar component

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addCity } from "../store/weatherSlice";
 
 function SearchBar() {
   const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (search.trim()) {
+      dispatch(
+        addCity({
+          id: search.toLowerCase(),
+          name: search.trim(),
+        }),
+      );
+
+      setSearch("");
+    }
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         value={search}
@@ -12,7 +30,7 @@ function SearchBar() {
         placeholder="Add a city"
         className="rounded-md border border-gray-600 p-2"
       />
-    </div>
+    </form>
   );
 }
 
